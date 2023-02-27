7b5baa66a395b7284f484b47de6fc0ba5d6c9ecc
孙亚洲 <yazhou.sun@kuwo.cn>
2023-02-27 10:06:18 +0800

    Initial Commit

---
+++ SafeCastDemo/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  SafeCastDemo
+//
+//  Created by sunyazhou on 2023/2/27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
